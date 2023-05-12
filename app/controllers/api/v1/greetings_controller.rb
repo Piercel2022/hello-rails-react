54b0f8b291faf0e858c2eefbd46e9ad1a4c4973c
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2023-05-12 15:44:46 +0200

    Fix errors

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,10 +1,10 @@
 module Api
-    module V1
-      class GreetingsController < ApplicationController
-        def index
-          greeting = Greeting.order('RANDOM()').first
-          render json: { greeting: greeting.message }
-        end
+  module V1
+    class GreetingsController < ApplicationController
+      def index
+        greeting = Greeting.order('RANDOM()').first
+        render json: { greeting: greeting.message }
       end
     end
+  end
 end
